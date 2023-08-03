9095c11025004c05ffb3b727d18bc4577dcbe756
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2023-08-03 19:19:11 +0200

    feat(hello world): initial commit, spring boot with thymeleaf

---
+++ src/main/java/com/start/helm/IndexController.java
@@ -0,0 +1,17 @@
+package com.start.helm;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+  @GetMapping("/")
+  public String index(Model model) {
+    model.addAttribute("message", "hello world");
+    return "index";
+  }
+
+}
---
+++ src/main/java/com/start/helm/StartHelmApp.java
@@ -0,0 +1,13 @@
+package com.start.helm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartHelmApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartHelmApp.class, args);
+	}
+
+}
