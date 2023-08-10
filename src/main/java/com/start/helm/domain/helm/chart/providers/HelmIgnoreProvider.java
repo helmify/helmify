45ebb73c9f1e0123eb981ed87fbe36731d783a2c
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2023-08-10 23:55:03 +0200

    impl(build helm chart): add logic to create zip file

---
+++ src/main/java/com/start/helm/domain/helm/chart/providers/HelmIgnoreProvider.java
@@ -0,0 +1,37 @@
+package com.start.helm.domain.helm.chart.providers;
+
+import com.start.helm.domain.helm.HelmContext;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HelmIgnoreProvider implements HelmFileProvider {
+
+  private static final String template = """
+      .DS_Store
+      # Common VCS dirs
+      .git/
+      .gitignore
+      .bzr/
+      .bzrignore
+      .hg/
+      .hgignore
+      .svn/
+      # Common backup files
+      *.swp
+      *.bak
+      *.tmp
+      *.orig
+      *~
+      # Various IDEs
+      .project
+      .idea/
+      *.tmproj
+      .vscode/
+            
+      """;
+
+  @Override
+  public String getFileContent(HelmContext context) {
+    return template;
+  }
+}
