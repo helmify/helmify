3d030a11855e779460f543cafccd910b22c52b4b
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2023-08-14 02:56:13 +0200

    chore(cleanup, docs): added comments, some cleanup

--- src/main/java/com/start/helm/domain/helm/chart/model/InitContainer.java
+++ src/main/java/com/start/helm/domain/helm/chart/model/InitContainer.java
@@ -14,48 +14,10 @@ import lombok.Setter;
 @AllArgsConstructor
 @EqualsAndHashCode
 public class InitContainer {
-  String name;
-  String image;
-  String imagePullPolicy;
-  Map<String, Object> securityContext;
-  List<String> command;
-  Map<String, Object> resources;
-
-    public static InitContainer prepare(String appName, String dependencyName) {
-
-      String nameTemplate = "\"{{ include \"%s.fullname\" . }}-%schecker\"";
-
-      return new InitContainer(
-          String.format(nameTemplate, appName, dependencyName),
-          "docker.io/busybox:stable",
-            "Always",
-            Map.of(
-                "allowPrivilegeEscalation", false,
-                "runAsUser", 1000,
-                "runAsGroup", 1000,
-                "runAsNonRoot", true
-            ),
-            List.of(
-                "sh",
-                "-c",
-                """
-                echo 'Waiting for %s to become ready...'
-                until printf "." && nc -z -w 2 {{ .Values.global.hosts.%s }} {{ .Values.global.ports.%s }}; do
-                    sleep 2;
-                done;
-                echo '%s OK ✓'
-                """.formatted(dependencyName, dependencyName, dependencyName, dependencyName)
-            ),
-            Map.of(
-                "requests", Map.of(
-                    "cpu", "20m",
-                    "memory", "32Mi"
-                ),
-                "limits", Map.of(
-                    "cpu", "20m",
-                    "memory", "32Mi"
-                )
-            )
-      );
-    }
-  }
+  private String name;
+  private String image;
+  private String imagePullPolicy;
+  private Map<String, Object> securityContext;
+  private List<String> command;
+  private Map<String, Object> resources;
+}
