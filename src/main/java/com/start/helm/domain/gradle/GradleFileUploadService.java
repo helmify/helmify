c606e191d1ef2ec9e826c68c3f5c34a497879a6b
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2023-09-22 21:18:35 +0200

    feat(spring initializr proxy): bugfix for processing gradle file

--- src/main/java/com/start/helm/domain/gradle/GradleFileUploadService.java
+++ src/main/java/com/start/helm/domain/gradle/GradleFileUploadService.java
@@ -4,60 +4,62 @@ import com.start.helm.domain.FileUploadService;
 import com.start.helm.domain.helm.HelmContext;
 import com.start.helm.domain.maven.resolvers.DependencyResolver;
 import com.start.helm.util.GradleUtil;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class GradleFileUploadService implements FileUploadService {
 
-  private final List<DependencyResolver> resolvers;
+    private final List<DependencyResolver> resolvers;
 
-  public HelmContext processBuildFile(String buildFile) {
-    return processBuildFile(buildFile, "my-project", GradleUtil.extractVersion(buildFile));
-  }
+    public HelmContext processBuildFile(String buildFile) {
+        return processBuildFile(buildFile, "my-project", GradleUtil.extractVersion(buildFile));
+    }
 
-  /**
-   * Method which accepts a build.gradle file as String and builds a {@link HelmContext} from it.
-   * <p/>
-   * A note on gradle handling:
-   * After looking into gradle's tooling api, this simpler approach has proven as effective
-   * in extracting the few bits of data we need to populate a {@link HelmContext}.
-   */
-  public HelmContext processBuildFile(String buildFile, String appName, String appVersion) {
-    log.info("Processing Gradle Build: {}", buildFile);
+    /**
+     * Method which accepts a build.gradle file as String and builds a {@link HelmContext} from it.
+     * <p/>
+     * A note on gradle handling:
+     * After looking into gradle's tooling api, this simpler approach has proven as effective
+     * in extracting the few bits of data we need to populate a {@link HelmContext}.
+     */
+    public HelmContext processBuildFile(String buildFile, String appName, String appVersion) {
+        log.info("Processing Gradle Build: {}", buildFile);
 
-    HelmContext context = new HelmContext();
+        HelmContext context = new HelmContext();
 
-    context.setAppName(appName);
-    context.setAppVersion(appVersion);
-    context.setDependencyDescriptor(buildFile);
+        context.setAppName(appName);
+        context.setAppVersion(appVersion);
+        context.setDependencyDescriptor(buildFile);
 
-    buildFile
-        .lines()
-        .map(String::trim)
-        .filter(line -> line.startsWith("implementation"))
-        .map(line -> line.replace("implementation", ""))
-        .map(line -> line.replace("'", ""))
-        .map(line -> line.replace("\"", ""))
-        .map(line -> line.replace("(", "").replace(")", ""))
-        .map(line -> line.split(":")[1])
-        .flatMap(artifactId -> resolvers
-            .stream()
-            .filter(matcher -> matcher.matches(artifactId))
-            .map(matcher -> matcher.resolveDependency(context))
-            .filter(Optional::isPresent)
-            .map(Optional::get)
-        )
-        .collect(Collectors.toSet())
-        .forEach(context::addHelmChartFragment);
+        buildFile
+                .lines()
+                .filter(line -> line.contains("implementation") || line.contains("runtimeOnly"))
+                .map(String::trim)
+                .map(line -> line.replace("runtimeOnly", ""))
+                .map(line -> line.replace("implementation", ""))
+                .map(line -> line.replace("'", ""))
+                .map(line -> line.replace("\"", ""))
+                .map(line -> line.replace("(", "").replace(")", ""))
+                .map(line -> line.split(":")[1])
+                .flatMap(artifactId -> resolvers
+                        .stream()
+                        .filter(matcher -> matcher.matches(artifactId))
+                        .map(matcher -> matcher.resolveDependency(context))
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                )
+                .collect(Collectors.toSet())
+                .forEach(context::addHelmChartFragment);
 
-    return context;
-  }
+        return context;
+    }
 
 }
