d56dd68f78858d125feaf9a453ec0eb58ac8739a
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2023-10-07 04:19:00 +0200

    cleanup after interface refactoring

--- src/main/java/com/start/helm/domain/maven/resolvers/PostgresDependencyResolver.java
+++ src/main/java/com/start/helm/domain/maven/resolvers/PostgresDependencyResolver.java
@@ -1,14 +1,12 @@
 package com.start.helm.domain.maven.resolvers;
 
-import static com.start.helm.util.HelmUtil.initContainer;
-import static com.start.helm.util.HelmUtil.makeSecretKeyRef;
-
-import com.start.helm.domain.helm.HelmChartSlice;
 import com.start.helm.domain.helm.HelmContext;
+import org.springframework.stereotype.Component;
+
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
-import org.springframework.stereotype.Component;
+
+import static com.start.helm.util.HelmUtil.makeSecretKeyRef;
 
 /**
  * Resolver for spring postgres dependency.
@@ -16,83 +14,67 @@ import org.springframework.stereotype.Component;
 @Component
 public class PostgresDependencyResolver implements DependencyResolver {
 
-  @Override
-  public List<String> matchOn() {
-    return List.of("postgres");
-  }
-
-  /**
-   * HelmChartSlice for Postgres.
-   */
-  @Override
-  public Optional<HelmChartSlice> resolveDependency(HelmContext context) {
-    HelmChartSlice slice = new HelmChartSlice();
-
-    slice.setPreferredChart(getPreferredChart());
-    slice.setValuesEntries(getValuesEntries(context));
-    slice.setInitContainer(initContainer(context.getAppName(), dependencyName()));
-    slice.setDefaultConfig(getDefaultConfig());
-    slice.setSecretEntries(getSecretEntries());
-    slice.setEnvironmentEntries(getEnvironmentEntries(context));
-
-    return Optional.of(slice);
-  }
-
-  private List<Map<String, Object>> getEnvironmentEntries(HelmContext context) {
-    return List.of(
-        makeSecretKeyRef("SPRING_DATASOURCE_USERNAME", "postgres-username", context.getAppName()),
-        makeSecretKeyRef("SPRING_DATASOURCE_PASSWORD", "postgres-password", context.getAppName())
-    );
-  }
+    @Override
+    public List<String> matchOn() {
+        return List.of("postgres");
+    }
 
 
-  private Map<String, Object> getSecretEntries() {
-    return Map.of(
-        "postgres-username", "{{ .Values.postgresql.auth.username | b64enc | quote }}"
-        , "postgres-password", "{{ .Values.postgresql.auth.password | b64enc | quote }}"
-    );
-  }
+    public List<Map<String, Object>> getEnvironmentEntries(HelmContext context) {
+        return List.of(
+                makeSecretKeyRef("SPRING_DATASOURCE_USERNAME", "postgres-username", context.getAppName()),
+                makeSecretKeyRef("SPRING_DATASOURCE_PASSWORD", "postgres-password", context.getAppName())
+        );
+    }
 
 
-  private Map<String, String> getDefaultConfig() {
-    return Map.of(
-        "spring.datasource.url",
-        "jdbc:postgresql://{{ .Values.global.hosts.postgresql }}:{{ .Values.global.ports.postgresql }}/{{ .Values.postgresql.database }}"
-    );
-  }
+    public Map<String, Object> getSecretEntries() {
+        return Map.of(
+                "postgres-username", "{{ .Values.postgresql.auth.username | b64enc | quote }}"
+                , "postgres-password", "{{ .Values.postgresql.auth.password | b64enc | quote }}"
+        );
+    }
 
-  private Map<String, Object> getValuesEntries(HelmContext context) {
-    return Map.of("postgresql", Map.of(
-            "enabled", true,
-            "database", "postgres",
-            "fullnameOverride", context.getAppName() + "-postgresql",
-            "nameOverride", context.getAppName() + "-postgresql",
-            "architecture", "standalone",
-            "primary", Map.of(
-                "persistence", Map.of(
-                    "enabled", true,
-                    "storageClass", "",
-                    "accessModes", List.of("ReadWriteOnce"),
-                    "size", "1Gi"
-                )
-            ),
-            "auth",
-            Map.of("username", "postgres", "password", "postgres")),
-        "global",
-        Map.of("hosts", Map.of("postgresql", context.getAppName() + "-postgresql"), "ports", Map.of("postgresql", 5432))
-    );
-  }
 
-  private Map<String, String> getPreferredChart() {
-    return Map.of(
-        "name", "postgresql",
-        "version", "11.9.2",
-        "repository", "https://charts.bitnami.com/bitnami"
-    );
-  }
+    public Map<String, String> getDefaultConfig() {
+        return Map.of(
+                "spring.datasource.url",
+                "jdbc:postgresql://{{ .Values.global.hosts.postgresql }}:{{ .Values.global.ports.postgresql }}/{{ .Values.postgresql.database }}"
+        );
+    }
 
-  @Override
-  public String dependencyName() {
-    return "postgresql";
-  }
+    public Map<String, Object> getValuesEntries(HelmContext context) {
+        return Map.of("postgresql", Map.of(
+                        "enabled", true,
+                        "database", "postgres",
+                        "fullnameOverride", context.getAppName() + "-postgresql",
+                        "nameOverride", context.getAppName() + "-postgresql",
+                        "architecture", "standalone",
+                        "primary", Map.of(
+                                "persistence", Map.of(
+                                        "enabled", true,
+                                        "storageClass", "",
+                                        "accessModes", List.of("ReadWriteOnce"),
+                                        "size", "1Gi"
+                                )
+                        ),
+                        "auth",
+                        Map.of("username", "postgres", "password", "postgres")),
+                "global",
+                Map.of("hosts", Map.of("postgresql", context.getAppName() + "-postgresql"), "ports", Map.of("postgresql", 5432))
+        );
+    }
+
+    public Map<String, String> getPreferredChart() {
+        return Map.of(
+                "name", "postgresql",
+                "version", "11.9.2",
+                "repository", "https://charts.bitnami.com/bitnami"
+        );
+    }
+
+    @Override
+    public String dependencyName() {
+        return "postgresql";
+    }
 }
