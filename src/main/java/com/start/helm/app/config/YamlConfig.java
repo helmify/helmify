4d24baf5f298431c1cd76df935ecee210b6a0137
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2023-10-18 22:21:35 +0200

    Feature/graalvm (#41)
    * feat(graalvm): use spring boot 3.2.0.m3, graalvm, java21
    
    * fix(build)
    
    * fix(runtime config)
    
    * fix(mount path)
    
    * fix(build)
    
    * fix(build)
    
    * fix(build)
    
    * feat(data dir configurable)

--- src/main/java/com/start/helm/app/config/YamlConfig.java
+++ src/main/java/com/start/helm/app/config/YamlConfig.java
@@ -4,25 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.yaml.snakeyaml.DumperOptions;
 import org.yaml.snakeyaml.Yaml;
-import org.yaml.snakeyaml.introspector.Property;
-import org.yaml.snakeyaml.nodes.NodeTuple;
-import org.yaml.snakeyaml.nodes.Tag;
-import org.yaml.snakeyaml.representer.Representer;
 
 @Configuration
 public class YamlConfig {
 
-    private static final Representer nonNullRepresenter = new Representer() {
-        @Override
-        protected NodeTuple representJavaBeanProperty(Object javaBean, Property property, Object propertyValue, Tag customTag) {
-            if (propertyValue == null) {
-                return null;
-            } else {
-                return super.representJavaBeanProperty(javaBean, property, propertyValue, customTag);
-            }
-        }
-    };
-    private static final Yaml yaml = new Yaml(nonNullRepresenter, getOptions());
+
+    private static final Yaml yaml = new Yaml(getOptions());
 
     private static DumperOptions getOptions() {
         DumperOptions options = new DumperOptions();
