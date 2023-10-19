0767b27e14df7f3c69dd11f5fea22cec4b4e9c1b
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2023-10-19 17:55:04 +0200

    Feature/40 support mysql (#44)
    * Develop (#21)
    
    * chore(readme): some docs
    
    * chore(build): add test goal to build
    
    * fix(test): were not running at all..
    
    * fix(build)
    
    * fix(build)
    
    * impl(inspect dependencies): added matchers for dependencies and logic for helm package lookup. also, package refactoring
    
    * impl(inspect dependencies):  package refactoring
    
    * impl(build helm chart): added model classes for values.yaml and Chart.yaml
    
    * impl(build helm chart): added providers for helm files
    
    * impl(build helm chart): add logic to create zip file
    
    * impl(deployment patch): add logic to patch in env vars
    
    * impl(deployment patch): add logic to patch in env vars, init containers
    
    * chore(cleanup, docs): added comments, some cleanup
    
    * chore(cleanup, docs): added comments, some cleanup
    
    * 10 handle postgres (#15)
    
    * fix(rabbitmq values): added missing values.yaml sections
    
    * impl(postgres resolver): add resolver for postgres
    
    * impl(postgres resolver): add resolver for postgres, merge global config blocks
    
    * impl(postgres resolver): added some tests
    
    * impl(ui): add layout w/ pom details, app summary, customization form
    
    * impl(ui): add about
    
    * impl(ui): add about
    
    * impl(download): add download for helm.zip
    
    * fix(test)
    
    * fix(test)
    
    * fix(configmap)
    
    * fix(naming)
    
    * fix(naming)
    
    * fix(config mount)
    
    * chore(comments): javadoc comments
    
    * testing
    
    * rename
    
    * rename
    
    ---------
    
    Co-authored-by: Pascal Salg <pascal.salg@vitaliberty.de>
    
    * release 0.2.0
    
    * update README.md
    
    * feat(mysql support): add resolver for mysql support
    
    * feat(mysql support): use default db
    
    * feat(mysql support): add test
    
    ---------
    
    Co-authored-by: Pascal Salg <pascal.salg@vitaliberty.de>

---
+++ src/main/java/com/start/helm/domain/resolvers/MySQLDependencyResolver.java
@@ -0,0 +1,80 @@
+package com.start.helm.domain.resolvers;
+
+import com.start.helm.domain.helm.HelmContext;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.start.helm.util.HelmUtil.makeSecretKeyRef;
+
+/**
+ * Resolver for spring postgres dependency.
+ */
+@Component
+public class MySQLDependencyResolver implements DependencyResolver {
+
+    @Override
+    public List<String> matchOn() {
+        return List.of("mysql-connector-j");
+    }
+
+
+    public List<Map<String, Object>> getEnvironmentEntries(HelmContext context) {
+        return List.of(
+                makeSecretKeyRef("SPRING_DATASOURCE_USERNAME", "mysql-username", context.getAppName()),
+                makeSecretKeyRef("SPRING_DATASOURCE_PASSWORD", "mysql-password", context.getAppName())
+        );
+    }
+
+
+    public Map<String, Object> getSecretEntries() {
+        return Map.of(
+                "mysql-username", "{{ .Values.mysql.auth.username | b64enc | quote }}"
+                , "mysql-password", "{{ .Values.mysql.auth.password | b64enc | quote }}"
+        );
+    }
+
+
+    public Map<String, String> getDefaultConfig() {
+        return Map.of(
+                "spring.datasource.url",
+                "jdbc:mysql://{{ .Values.global.hosts.mysql }}:{{ .Values.global.ports.mysql }}/{{ .Values.mysql.database }}"
+        );
+    }
+
+    public Map<String, Object> getValuesEntries(HelmContext context) {
+        return Map.of("mysql", Map.of(
+                        "enabled", true,
+                        "database", "my_database",
+                        "fullnameOverride", context.getAppName() + "-mysql",
+                        "nameOverride", context.getAppName() + "-mysql",
+                        "architecture", "standalone",
+                        "primary", Map.of(
+                                "persistence", Map.of(
+                                        "enabled", true,
+                                        "storageClass", "",
+                                        "accessModes", List.of("ReadWriteOnce"),
+                                        "size", "1Gi"
+                                )
+                        ),
+                        "auth",
+                        Map.of("username", "mysql", "password", "mysql", "rootPassword", "mysql")),
+                "global",
+                Map.of("hosts", Map.of("mysql", context.getAppName() + "-mysql"), "ports", Map.of("mysql", 3306))
+        );
+    }
+
+    public Map<String, String> getPreferredChart() {
+        return Map.of(
+                "name", "mysql",
+                "version", "9.12.5",
+                "repository", "https://charts.bitnami.com/bitnami"
+        );
+    }
+
+    @Override
+    public String dependencyName() {
+        return "mysql";
+    }
+}
