6e5dda076715f87607ba2e1160fb9081ad5ab93e
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2023-10-23 23:54:44 +0200

    fix formatting

--- src/main/java/com/start/helm/domain/resolvers/RabbitmqDependencyResolver.java
+++ src/main/java/com/start/helm/domain/resolvers/RabbitmqDependencyResolver.java
@@ -16,6 +16,8 @@ import static com.start.helm.util.HelmUtil.makeSecretKeyRef;
 @Component
 public class RabbitmqDependencyResolver implements DependencyResolver {
 
+	//@formatter:off
+
 	@Override
 	public String dependencyName() {
 		return "rabbitmq";
@@ -27,31 +29,47 @@ public class RabbitmqDependencyResolver implements DependencyResolver {
 	}
 
 	public Map<String, Object> getSecretEntries() {
-		return Map.of("rabbitmq-username", "{{ .Values.rabbitmq.auth.username | b64enc | quote }}", "rabbitmq-password",
-				"{{ .Values.rabbitmq.auth.password | b64enc | quote }}");
+		return Map.of(
+				"rabbitmq-username", "{{ .Values.rabbitmq.auth.username | b64enc | quote }}"
+				, "rabbitmq-password", "{{ .Values.rabbitmq.auth.password | b64enc | quote }}"
+		);
 	}
 
 	public Map<String, Object> getValuesEntries(HelmContext context) {
-		return Map.of("rabbitmq",
-				Map.of("enabled", true, "port", 5672, "vhost", "/", "nameOverride", context.getAppName() + "-rabbitmq",
-						"fullnameOverride", context.getAppName() + "-rabbitmq", "auth",
-						Map.of("username", "guest", "password", "guest")),
-				"global", Map.of("hosts", Map.of("rabbitmq", context.getAppName() + "-rabbitmq"), "ports",
-						Map.of("rabbitmq", 5672)));
+		return Map.of(
+				"rabbitmq",
+				Map.of("enabled", true,
+						"port", 5672,
+						"vhost", "/",
+						"nameOverride", context.getAppName() + "-rabbitmq",
+						"fullnameOverride", context.getAppName() + "-rabbitmq",
+						"auth", Map.of("username", "guest", "password", "guest")
+				),
+				"global", Map.of("hosts", Map.of("rabbitmq", context.getAppName() + "-rabbitmq"), "ports", Map.of("rabbitmq", 5672))
+		);
 	}
 
 	public Map<String, String> getPreferredChart() {
-		return Map.of("name", "rabbitmq", "version", "11.9.0", "repository", "https://charts.bitnami.com/bitnami");
+		return Map.of(
+				"name", "rabbitmq",
+				"version", "11.9.0",
+				"repository", "https://charts.bitnami.com/bitnami"
+		);
 	}
 
 	public Map<String, String> getDefaultConfig() {
-		return Map.of("spring.rabbitmq.host", "{{ .Values.global.hosts.rabbitmq }}", "spring.rabbitmq.port",
-				"{{ .Values.global.ports.rabbitmq }}", "spring.rabbitmq.virtual-host", "{{ .Values.rabbitmq.vhost }}");
+		return Map.of(
+				"spring.rabbitmq.host", "{{ .Values.global.hosts.rabbitmq }}",
+				"spring.rabbitmq.port", "{{ .Values.global.ports.rabbitmq }}",
+				"spring.rabbitmq.virtual-host", "{{ .Values.rabbitmq.vhost }}"
+		);
 	}
 
 	public List<Map<String, Object>> getEnvironmentEntries(HelmContext context) {
-		return List.of(makeSecretKeyRef("SPRING_RABBITMQ_USERNAME", "rabbitmq-username", context.getAppName()),
-				makeSecretKeyRef("SPRING_RABBITMQ_PASSWORD", "rabbitmq-password", context.getAppName()));
+		return List.of(
+				makeSecretKeyRef("SPRING_RABBITMQ_USERNAME", "rabbitmq-username", context.getAppName()),
+				makeSecretKeyRef("SPRING_RABBITMQ_PASSWORD", "rabbitmq-password", context.getAppName())
+		);
 	}
 
 }
