bc1fa05d8f72659cf2897a631489d993705616f7
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2023-10-24 02:25:10 +0200

    add service to clean up tmp dir

---
+++ src/main/java/com/start/helm/app/config/SchedulingConfig.java
@@ -0,0 +1,10 @@
+package com.start.helm.app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+
+}
