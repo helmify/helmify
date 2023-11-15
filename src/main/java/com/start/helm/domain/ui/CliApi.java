0fff9fc7b8c301d3525f6733c956bb9e4ed6bfd3
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2023-11-15 01:51:24 +0100

    feat(cli): cleanup, track cli chart downloads

--- src/main/java/com/start/helm/domain/ui/CliApi.java
+++ src/main/java/com/start/helm/domain/ui/CliApi.java
@@ -1,5 +1,6 @@
 package com.start.helm.domain.ui;
 
+import com.start.helm.domain.events.ChartDownloadedEvent;
 import com.start.helm.domain.gradle.GradleFileUploadService;
 import com.start.helm.domain.helm.HelmContext;
 import com.start.helm.domain.helm.chart.HelmChartService;
@@ -7,6 +8,7 @@ import com.start.helm.domain.maven.MavenFileUploadService;
 import com.start.helm.util.DownloadUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,19 +25,20 @@ public class CliApi {
 
 	private final GradleFileUploadService gradleFileUploadService;
 
+	private final ApplicationEventPublisher publisher;
+
+
 	@PostMapping("/cli")
-	public Object cli(@RequestParam("name") String name, @RequestParam("version") String version,
+	public ResponseEntity<byte[]> cli(@RequestParam("name") String name, @RequestParam("version") String version,
 			@RequestBody String buildFileContents) {
 
-		log.info("Got request for name: {}", name);
-		log.info("Got request for version: {}", version);
-		log.info("Got request for buildFileContents: {}", buildFileContents);
-
 		HelmContext helmContext = buildFileContents.contains("<groupId>")
-				? mavenFileUploadService.processBuildFile(buildFileContents)
-				: gradleFileUploadService.processBuildFile(buildFileContents);
+				? mavenFileUploadService.processBuildFile(buildFileContents, name, version)
+				: gradleFileUploadService.processBuildFile(buildFileContents, name, version);
 
 		byte[] resource = helmChartService.process(helmContext);
+		publisher.publishEvent(new ChartDownloadedEvent());
+
 		return ResponseEntity.ok()
 			.headers(DownloadUtil.headers("helm.zip"))
 			.contentLength(resource.length)
