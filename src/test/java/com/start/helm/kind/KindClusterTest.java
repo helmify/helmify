c94e78b3517529d3ba9e5ed0b2688ff1f16c0b45
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-01-31 14:26:09 +0100

    testing(helm charts): add cluster test

--- src/test/java/com/start/helm/kind/KindClusterTest.java
+++ src/test/java/com/start/helm/kind/KindClusterTest.java
@@ -2,25 +2,43 @@ package com.start.helm.kind;
 
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@Slf4j
 public class KindClusterTest {
 
-    @Test
-    public void testCluster (){
-        try (KubernetesClient client = new DefaultKubernetesClient()){
-            System.out.println("Services:");
-            client.services().list().getItems().stream().map(service -> service.getMetadata().getName()).forEach(System.out::println);
-            System.out.println("-----------------------------------");
-            System.out.println("Pods:");
-            client.pods().list().getItems().stream().map(pod -> pod.getMetadata().getName()).forEach(System.out::println);
-            System.out.println("-----------------------------------");
-            System.out.println("Deployments:");
-            client.apps().deployments().list().getItems().stream().map(deployment -> deployment.getMetadata().getName()).forEach(System.out::println);
-        }
-    }
-
+	@Test
+	public void testCluster() {
+		try (KubernetesClient client = new DefaultKubernetesClient()) {
+			System.out.println("Services:");
+			client.services()
+				.list()
+				.getItems()
+				.stream()
+				.map(service -> service.getMetadata().getName())
+				.forEach(System.out::println);
+			System.out.println("-----------------------------------");
+			System.out.println("Pods:");
+			client.pods()
+				.list()
+				.getItems()
+				.stream()
+				.map(pod -> pod.getMetadata().getName())
+				.forEach(System.out::println);
+			System.out.println("-----------------------------------");
+			System.out.println("Deployments:");
+			client.apps()
+				.deployments()
+				.list()
+				.getItems()
+				.stream()
+				.map(deployment -> deployment.getMetadata().getName())
+				.forEach(System.out::println);
+		}
+		catch (Exception e) {
+			log.error("Error getting cluster info", e);
+		}
+	}
 
 }
