8430eb8a8acb900bc5d1081b429a1e8ab041c00b
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-01-31 11:26:21 +0100

    testing(helm charts): added test for redis

---
+++ src/test/java/com/start/helm/domain/helm/chart/tests/RedisChartTest.java
@@ -0,0 +1,30 @@
+package com.start.helm.domain.helm.chart.tests;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class RedisChartTest extends HelmChartTests {
+
+	@Test
+	public void testChartWithRedis() {
+
+		List<String> starterDependencies = List.of("data-redis");
+		//@formatter:off
+        List<String> unittestFiles = List.of(
+                "deployment_redis_test.yaml",
+                "service_redis_test.yaml",
+                "configmap_redis_test.yaml"
+        );
+
+        lintAndTestChart(
+                new HelmUnittestContext(
+                        "test-redis-chart",
+                        "1.0.0",
+                        starterDependencies,
+                        "redis",
+                        unittestFiles)
+        );
+    }
+
+}
