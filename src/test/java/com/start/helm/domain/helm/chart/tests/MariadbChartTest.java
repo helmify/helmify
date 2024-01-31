0c5b5bfc0c12b3b3e399511cc25e87dd3ddbe1f2
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-01-31 11:25:34 +0100

    testing(helm charts): added test for mariadb

---
+++ src/test/java/com/start/helm/domain/helm/chart/tests/MariadbChartTest.java
@@ -0,0 +1,31 @@
+package com.start.helm.domain.helm.chart.tests;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+public class MariadbChartTest extends HelmChartTests {
+
+	@Test
+	public void testChartWithMariadb() {
+
+		List<String> starterDependencies = List.of("mariadb");
+		//@formatter:off
+        List<String> unittestFiles = List.of(
+                "deployment_mariadb_test.yaml",
+                "service_mariadb_test.yaml",
+                "secrets_mariadb_test.yaml",
+                "configmap_mariadb_test.yaml"
+        );
+
+        lintAndTestChart(
+                new HelmUnittestContext(
+                        "test-mariadb-chart",
+                        "1.0.0",
+                        starterDependencies,
+                        "mariadb",
+                        unittestFiles)
+        );
+    }
+
+}
