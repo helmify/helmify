fc8e4abc52637dd6d15c4dd7b81d08e0d490c474
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-02-09 00:32:46 +0100

    quarkus(initial support): add quarkus initializr proxy support

--- src/main/java/com/start/helm/initializr/spring/SpringInitializrProxy.java
+++ src/main/java/com/start/helm/initializr/spring/SpringInitializrProxy.java
@@ -1,15 +1,9 @@
 package com.start.helm.initializr.spring;
 
-import com.start.helm.domain.events.ChartDownloadedEvent;
-import com.start.helm.domain.gradle.GradleFileUploadService;
-import com.start.helm.domain.helm.chart.HelmChartService;
-import com.start.helm.domain.maven.MavenFileUploadService;
+import com.start.helm.initializr.InitializrSupport;
 import com.start.helm.util.DownloadUtil;
-import com.start.helm.util.GradleUtil;
-import com.start.helm.util.ZipUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.core.io.ByteArrayResource;
@@ -20,24 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.*;
+import java.io.IOException;
 import java.net.URI;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
-import java.util.zip.ZipInputStream;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
 public class SpringInitializrProxy {
 
-	private final MavenFileUploadService mavenFileUploadService;
-
-	private final GradleFileUploadService gradleFileUploadService;
-
-	private final HelmChartService helmChartService;
+	private final InitializrSupport initializrSupport;
 
 	private final ApplicationEventPublisher publisher;
 
@@ -71,27 +60,7 @@ public class SpringInitializrProxy {
 
 		if (forEntity.getStatusCode().is2xxSuccessful() && forEntity.getBody() != null) {
 
-			byte[] body = forEntity.getBody();
-			String uuid = UUID.randomUUID().toString();
-			Path dataDirectory = Paths.get("helm-start-data");
-			File parentDir = Paths.get(dataDirectory.toFile().getAbsolutePath(), "tmp", uuid).toFile();
-			parentDir.mkdirs();
-			File helmDir = Paths.get(parentDir.getAbsolutePath(), "helm").toFile();
-			helmDir.mkdirs();
-
-			org.zeroturnaround.zip.ZipUtil.unpack(new ByteArrayInputStream(body), parentDir);
-
-			ByteArrayResource helmChart = this.generateHelmChart(new ByteArrayResource(body));
-
-			org.zeroturnaround.zip.ZipUtil.unpack(helmChart.getInputStream(), helmDir);
-
-			ByteArrayOutputStream outputStream = new ByteArrayOutputStream(body.length);
-
-			org.zeroturnaround.zip.ZipUtil.pack(parentDir, outputStream);
-
-			ByteArrayResource merged = new ByteArrayResource(outputStream.toByteArray());
-
-			publisher.publishEvent(new ChartDownloadedEvent());
+			ByteArrayResource merged = this.initializrSupport.repackStarter(forEntity.getBody());
 
 			return ResponseEntity.ok()
 				.headers(DownloadUtil.headers("starter.zip"))
@@ -101,68 +70,6 @@ public class SpringInitializrProxy {
 		}
 
 		return ResponseEntity.internalServerError().build();
-	}
-
-	@SneakyThrows
-	private ByteArrayResource generateHelmChart(ByteArrayResource body) {
-		byte[] content = body.getContentAsByteArray();
-
-		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(content.length);
-
-		Optional<String> buildFile = tryReadBuildFile(body.getInputStream());
-		buildFile.map(f -> {
-			if (f.trim().startsWith("<?xml")) {
-				return mavenFileUploadService.processBuildFile(f);
-			}
-			else {
-				try {
-					String appName = getGradleAppName(body.getInputStream());
-					return gradleFileUploadService.processBuildFile(f, appName, GradleUtil.extractVersion(f));
-				}
-				catch (Exception e) {
-					log.error("Error processing build file", e);
-				}
-				return null;
-			}
-		}).map(helmContext -> this.helmChartService.process(helmContext, outputStream, true)).orElseThrow();
-
-		return new ByteArrayResource(outputStream.toByteArray());
-	}
-
-	private String getGradleAppName(InputStream inputStream) {
-		resetStream(inputStream);
-		Optional<String> zipContent = ZipUtil.getZipContent("settings.gradle", new ZipInputStream(inputStream));
-		return zipContent.map(s -> s.replace("rootProject.name = ", ""))
-			.map(SpringInitializrProxy::cleanupAppName)
-			.orElse("my-project");
-	}
-
-	private static String cleanupAppName(String name) {
-		return name.replace("'", "")
-			.replaceAll("[^\\x00-\\x7F]", "")
-			.replaceAll("[\\p{Cntrl}&&[^\r\n\t]]", "")
-			.replaceAll("\\p{C}", "");
-	}
-
-	@SneakyThrows
-	private void resetStream(InputStream zipInputStream) {
-		try {
-			zipInputStream.reset();
-		}
-		catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	private Optional<String> tryReadBuildFile(InputStream zipInputStream) {
-		return Optional
-			.ofNullable(ZipUtil.getZipContent("build.gradle.kts", new ZipInputStream(zipInputStream)).orElseGet(() -> {
-				resetStream(zipInputStream);
-				return ZipUtil.getZipContent("build.gradle", new ZipInputStream(zipInputStream)).orElseGet(() -> {
-					resetStream(zipInputStream);
-					return ZipUtil.getZipContent("pom.xml", new ZipInputStream(zipInputStream)).orElse(null);
-				});
-			}));
 	}
 
 }
