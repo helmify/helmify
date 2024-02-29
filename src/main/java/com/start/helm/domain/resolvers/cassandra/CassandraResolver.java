ff299b65bb05e9aa21f6d7c27731b96a9a747108
psalg-dev <129699997+psalg-dev@users.noreply.github.com>
2024-02-29 22:24:29 +0100

    impl(cassandra support with spring)

---
+++ src/main/java/com/start/helm/domain/resolvers/cassandra/CassandraResolver.java
@@ -0,0 +1,53 @@
+package com.start.helm.domain.resolvers.cassandra;
+
+import com.start.helm.domain.helm.HelmContext;
+import com.start.helm.domain.resolvers.DependencyResolver;
+
+import java.util.Map;
+
+public interface CassandraResolver extends DependencyResolver {
+
+	//@formatter:off
+
+	default Map<String, Object> getValuesEntries(HelmContext context) {
+		return Map.of("cassandra",
+				Map.of("enabled", true,
+						"keyspaceName", context.getAppName(),
+						"service", Map.of(
+								"ports", Map.of(
+										"cql", getPort()
+								)
+							)
+						),
+						"port", getPort(),
+						"nameOverride", context.getAppName() + "-cassandra",
+						"fullnameOverride", context.getAppName() + "-cassandra",
+						"dbUser", Map.of(
+								"user", "cassandra",
+							"password", "cassandra"
+						),
+						"global", Map.of(
+								"hosts", Map.of(
+										"cassandra", context.getAppName() + "-cassandra"),
+							"ports", Map.of(
+									"cassandra", getPort())));
+	}
+
+	default Map<String, String> getPreferredChart() {
+		return Map.of(
+				"name", "cassandra",
+				"version", "4.1.4",
+				"repository", "https://charts.bitnami.com/bitnami"
+		);
+	}
+
+	default int getPort() {
+		return 9042;
+	}
+
+	@Override
+	default String dependencyName() {
+		return "cassandra";
+	}
+
+}
---
+++ src/main/java/com/start/helm/domain/resolvers/cassandra/spring/SgCassandraResolver.java
@@ -0,0 +1,45 @@
+package com.start.helm.domain.resolvers.cassandra.spring;
+
+import com.start.helm.domain.helm.HelmContext;
+import com.start.helm.domain.resolvers.cassandra.CassandraResolver;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+
+import static com.start.helm.util.HelmUtil.makeSecretKeyRef;
+
+@Component
+public class SgCassandraResolver implements CassandraResolver {
+
+	//@formatter:off
+    @Override
+    public List<String> matchOn() {
+        return List.of("spring-boot-starter-data-cassandra", "spring-boot-starter-data-cassandra-reactive");
+    }
+
+
+    public List<Map<String, Object>> getEnvironmentEntries(HelmContext context) {
+        return List.of(
+                makeSecretKeyRef("SPRING_CASSANDRA_USERNAME", "cassandra-username", context.getAppName()),
+                makeSecretKeyRef("SPRING_CASSANDRA_PASSWORD", "cassandra-password", context.getAppName())
+        );
+    }
+
+
+    public Map<String, Object> getSecretEntries() {
+        return Map.of(
+                "cassandra-username", "{{ .Values.cassandra.dbUser.user | b64enc | quote }}"
+                , "cassandra-password", "{{ .Values.cassandra.dbUser.password | b64enc | quote }}"
+        );
+    }
+
+
+    public Map<String, String> getDefaultConfig() {
+        return Map.of(
+                "spring.cassandra.contact-points","{{ .Values.global.hosts.cassandra }}:{{ .Values.global.ports.cassandra }}",
+                "spring.cassandra.keyspace-name", "{{ .Values.cassandra.keyspaceName }}"
+        );
+    }
+
+}
